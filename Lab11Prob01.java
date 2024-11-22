a2a59f25b0098fac9e261d3f8e0603e1cf5275dd
HMullis465 <hm11732@georgiasouthern.edu>
2024-11-22 15:24:30 -0500

    Update Lab11Prob01.java
    Works

--- Lab11Prob01.java
+++ Lab11Prob01.java
@@ -6,24 +6,34 @@ import java.io.FileOutputStream;
 import java.io.IOException;
 
 /**
- * File: Lab11Prob01.java Class: CSCI 1302 Author: Hugh Mullis and Joseph Gibson
- * Created on: 11/22/2024 Description:
+ * File: Lab11Prob01.java 
+ * Class: CSCI 1302 
+ * Author: Hugh Mullis and Joseph Gibson
+ * Created on: 11/22/2024 
+ * Description: Satisfies the requirements for Lab 11 Problem 1
  */
 public class Lab11Prob01 {
 
 	public static void main(String[] args) {
 
-		try ( DataInputStream input = new DataInputStream(new FileInputStream("src/people.dat"));
-				DataOutputStream output = new DataOutputStream(new FileOutputStream("src/people-copy.dat"));
-			) {
+		// Create an input stream and output stream
+		try (DataInputStream input = new DataInputStream(new FileInputStream("src/people.dat"));
+				DataOutputStream output = new DataOutputStream(new FileOutputStream("src/people-copy.dat"));) {
+			// read date from file
 			while (true) {
-			System.out.println(input.readInt() + " " + input.readUTF() + " " + input.readUTF() + " " + input.readInt() + " " + input.readDouble());
-			
-			output.writeInt(input.readInt());
-			output.writeUTF(input.readUTF());
-			output.writeUTF(input.readUTF());
-			output.writeInt(input.readInt());
-			output.writeDouble(input.readDouble());
+				int age = input.readInt();
+				String name = input.readUTF();
+				String address = input.readUTF();
+				int zip = input.readInt();
+				double salary = input.readDouble();
+
+				// Print data to console and to output
+				System.out.printf("%d %s %s %d %.2f%n", age, name, address, zip, salary);
+				output.writeInt(age);
+				output.writeUTF(name);
+				output.writeUTF(address);
+				output.writeInt(zip);
+				output.writeDouble(salary);
 
 			}
 		} catch (IOException ex) {
